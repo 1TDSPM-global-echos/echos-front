de7f91836f4e84329549fafc49240087e12ea9a6
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-11-14 01:22:00 -0300

    responsividade do navbar atualizada

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     return (
         <nav className="bg-[#00000050] text-white shadow-lg fixed top-0 left-0 w-full z-10">
             <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-
+                
                 <Link href="/" className="flex items-center">
                     <Image
                         src={Logo}
@@ -24,23 +24,24 @@ export default function Navbar() {
                 </Link>
 
                 <button
-                    className="md:hidden text-white focus:outline-none"
+                    className="md:hidden text-white focus:outline-none z-20"
                     onClick={() => setIsOpen(!isOpen)}
                 >
                     {isOpen ? <FiX className="w-6 h-6" /> : <FiMenu className="w-6 h-6" />}
                 </button>
 
-                <div className={`md:flex space-x-8 ${isOpen ? 'block' : 'hidden'} mt-4 md:mt-0`}>
-                    <Link href="/sobre" className="linksnav">
+                {/* Links */}
+                <div className={`md:flex md:space-x-8 ${isOpen ? 'flex flex-col items-center bg-black bg-opacity-90 absolute top-0 left-0 h-screen w-full pt-20' : 'hidden md:block'} transition-all duration-300`}>
+                    <Link href="/sobre" className="linksnav py-2 md:py-0 hover:text-gray-400">
                         Sobre
                     </Link>
-                    <Link href="/projetos" className="linksnav">
+                    <Link href="/projetos" className="linksnav py-2 md:py-0 hover:text-gray-400">
                         Projetos
                     </Link>
-                    <Link href="/calculadora" className="linksnav">
+                    <Link href="/calculadora" className="linksnav py-2 md:py-0 hover:text-gray-400">
                         Calculadora
                     </Link>
-                    <Link href="/contato" className="linksnav">
+                    <Link href="/contato" className="linksnav py-2 md:py-0 hover:text-gray-400">
                         Contato
                     </Link>
                 </div>
