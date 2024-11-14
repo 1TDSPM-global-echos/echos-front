3e4b515cc38794c1f1fd0b926a76621a58c6ae4a
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-11-14 02:28:52 -0300

    mensagem de boas vindas adicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,23 @@ export default function Home() {
 
     return (
         <div className="bg-cover bg-center bg-no-repeat min-h-screen bg-[url('../assets/fundo-home.jpeg')] flex items-center justify-center">
-            <div className="mx-auto px-4 flex items-center justify-center max-w-lg w-full">
+            <div className="mx-auto px-4 flex flex-col md:flex-row items-center justify-center gap-8">
                 
-                <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center space-y-4 text-corCinza w-full">
+                <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center space-y-4 text-corCinza max-w-lg w-full">
                     <p className="text-lg font-semibold">{cards[cardAtual].text}</p>
                     <Link href={cards[cardAtual].link} passHref>
                         <button className="bg-corVerdeFolha text-white px-4 py-2 rounded-md hover:bg-corVerdeFolhaHover transition duration-300">
                             {cards[cardAtual].linkText}
                         </button>
                     </Link>
+                </div>
+
+                <div className="bg-black bg-opacity-60 p-6 rounded-lg text-corCinzaClaro text-center md:text-left max-w-lg w-full">
+                    <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Echos!</h1>
+                    <p className="text-lg">
+                        Nosso projeto busca conscientizar sobre o impacto ambiental dos hábitos diários e incentivar práticas mais sustentáveis.
+                        Explore nossos recursos e descubra como você pode fazer a diferença!
+                    </p>
                 </div>
 
             </div>
