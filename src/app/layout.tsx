1c1f77650dc0ed5066823b9d46b6f337da55245f
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-11-14 01:06:17 -0300

    imagem atualizada, fontes adicionadas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
 import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import "./globals.css";
+import { Quicksand, Open_Sans } from "next/font/google";
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-quicksand",
+});
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Echos",
@@ -16,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={`${quicksand.variable} ${openSans.variable}`}>
       <body>
         <Navbar />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-
 export default function Home() {
   return (
-    <div className="bg-cover bg-center bg-no-repeat min-h-screen bg-[url('../assets/fundo-home.jfif')]">
+    <div className="bg-cover bg-center bg-no-repeat min-h-screen bg-[url('../assets/fundo-home.jpeg')]">
       <div className="flex items-center justify-center h-screen text-white">
         <h1 className="text-5xl font-bold">Titulo</h1>
       </div>
