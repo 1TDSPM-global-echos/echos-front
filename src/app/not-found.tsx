53fff4de8059bd361d35abb4ca3ce89a239b0f22
felipeprometti <felipe.prometti@gmail.com>
2024-11-22 18:25:10 -0300

    Pagina not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,17 @@
-export default function NotFound() {
+'use client';
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const NotFound = () => {
+    const router = useRouter();
+
     return (
-        <main>Página não encontrada</main>
-    )
-}
+        <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-black text-white">
+            <h1 className="text-6xl font-bold mb-4">404</h1>
+            <h2 className="text-3xl font-semibold mb-8">Página Não Encontrada</h2>
+        </main>
+    );
+};
+
+export default NotFound;
